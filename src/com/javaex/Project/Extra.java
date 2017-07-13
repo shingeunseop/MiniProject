4a3becc2319c7ac2245a4b40f76bdf4206a2aa11
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-13 19:13:11 +0900

    미니 프로젝트

---
+++ src/com/javaex/Project/Extra.java
@@ -0,0 +1,15 @@
+package com.javaex.Project;
+
+public class Extra {
+	Profile p1=new Profile(1,"이정재","010-2222-2222","02-4332-9090");
+	Profile p2=new Profile(2,"정우성","011-0000-23451","02-6552-2341");
+	Profile p3=new Profile(3,"유재석","010-5555-5555","02-7552-9994");
+	Profile p4=new Profile(4,"이효리","011-9999-7777","02-900-9888");
+	book.add(p1);
+	book.add(p2);
+	book.add(p3);
+	book.add(p4);
+	
+	
+	String Part[]=str.split(",");
+}
--- src/com/javaex/Project/MiniPro.java
+++ src/com/javaex/Project/MiniPro.java
@@ -1,24 +1,79 @@
 package com.javaex.Project;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Scanner;
 
 public class MiniPro {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("*************************************");
 		System.out.println("*          전화번호 관리 프로그램                 *");
 		System.out.println("*************************************");
 		System.out.println("1.리스트  2.등록  3.삭제  4.검색  5.종료");
 		System.out.println("--------------------------------------");
 		
-		System.out.println(">>메뉴번호:");
-		Scanner sc=new Scanner(System.in);
+		Reader r=new FileReader("D:\\javaStudy\\파일\\phoneDB.txt");
+		BufferedReader br=new BufferedReader(r);
+		
+		
+		String str;
+		Profile[] profile=new Profile[5];
+		while(true) {
+		str=br.readLine();
+		
+		if(str==null) {
+			break;
+		}
+		
+		String Part[]=str.split(",");
+		
+		int j=0;
+		profile [j].setName(Part[0]);
+		profile [j].setHp(Part[1]);
+		profile [j].setCompany(Part[2]);
+		j++;
+				
+		}
+		
+		while(true) {		
+		System.out.print(">>메뉴번호:");
+		
 		int i=sc.nextInt();
 		
-		ArrayList<tel> []=new ArrayList<tel>();
+		switch(i) {
+		case 1:
+			System.out.println("<1.리스트>");
+			for(int k=0;k<profile.length;k++) {
+				System.out.println(k+1+". "+profile[k].getName()+"      "
+			+profile[k].getHp()+"   "
+						+profile[k].getCompany());
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+			default:
+				System.out.println("다시 입력해주세요");
+				continue;
+		}
+		
+		}
 		
 		
-		sc.close();
+		
+		
+	
 		
 
 	}
